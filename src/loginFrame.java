e3932948f034d0a30e07dbc4fdb99f5faa895c45
ClassicM4n <118704585+ClassicM4n@users.noreply.github.com>
2022-12-13 14:56:48 +0700

    Update loginFrame.java

--- src/loginFrame.java
+++ src/loginFrame.java
@@ -11,6 +11,7 @@ import src.registerakun;
  * @author hilal
  * @server adiyaksa
  */
+//Import yang cukup penting
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -19,6 +20,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 public class loginFrame extends JFrame implements ActionListener{
+    //Komponen
     Container container =getContentPane();
     JLabel text= new JLabel("LOGIN SISWA");
     JButton login = new JButton("Login");
@@ -28,16 +30,18 @@ public class loginFrame extends JFrame implements ActionListener{
     JPasswordField password = new JPasswordField();
     JTextField username= new JTextField();
     JCheckBox checkbox=new JCheckBox("Show Password");
-    
+    //method untuk memulai LoginFrame
     loginFrame(){
         setLayoutManager();
         setLocationAndSize();
         initComponents();
         actionEvents();
     }
+    //Layout
     public void setLayoutManager(){
         container.setLayout(null);
     }
+    //Lokasi dan Size dari komponen
     public void setLocationAndSize(){
     user.setBounds(50,150,100,30);
     username.setBounds(150,150,150,30);
@@ -48,6 +52,7 @@ public class loginFrame extends JFrame implements ActionListener{
     registerButton.setBounds(170,300,100,30);
     checkbox.setBounds(150,260,150,30);
     }
+    //buat menambahkan komponen dalam JFrame
     public void initComponents(){
    container.add(username);
     container.add(user);
@@ -57,15 +62,17 @@ public class loginFrame extends JFrame implements ActionListener{
     container.add(registerButton);
     container.add(checkbox);
     }
+    //actionlistener dan windowlistener
     public void actionEvents(){
     login.addActionListener(this);
     registerButton.addActionListener(this);
     checkbox.addActionListener(this);
     addWindowListener(new exitclass());
     }
-
+//ActionEvent handler
     @Override
     public void actionPerformed(ActionEvent e){
+        //Tombol login di klik
         if(e.getSource()==login){
            String textNama,textPassword;
            textNama=username.getText();
@@ -80,13 +87,14 @@ public class loginFrame extends JFrame implements ActionListener{
              JOptionPane.showMessageDialog(this,"USERNAME/PASSWORD Yang anda masukkan salah");   
             }
         }
-        // Reset menjadi tombol register
+        // Tombol register di klik
         if(e.getSource()==registerButton){
           this.dispose();
           System.out.println("Passed Register Button");
           registerakun kontol = new registerakun();
           kontol.registerakun();
                   }
+        //Checkbox buat password
         if(e.getSource()==checkbox){ 
             if(checkbox.isSelected()){
                 password.setEchoChar((char)0);
@@ -96,6 +104,7 @@ public class loginFrame extends JFrame implements ActionListener{
             }
         }
     }
+    //WindowEvent handler
     private class exitclass extends WindowAdapter {
         @Override
         public void windowClosing(WindowEvent e){
