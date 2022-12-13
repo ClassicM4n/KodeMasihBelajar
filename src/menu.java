85d8d36768201db9bcadd9f42b59c4e13c68d345
ClassicM4n <118704585+ClassicM4n@users.noreply.github.com>
2022-12-13 15:20:07 +0700

    Update menu.java

--- src/menu.java
+++ src/menu.java
@@ -16,18 +16,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 public class menu extends JFrame implements ActionListener{
+   //Komponen JFrame
    Container container = getContentPane();
    JButton menu1=new JButton("CEK PROFIL");
    JButton menu2=new JButton("BANGUN DATAR");
    JLabel text= new JLabel("MENU APLIKASI");
    JButton exit=new JButton("EXIT");
    JButton menu3=new JButton("INPUT DATA SISWA");
-    public void menu(){
+   //Method utama 
+   public void menu(){
         LayoutManager();
         LocationAndSize();        
         components();
         actionevents();
     }
+   //Layout dan JFrame
     public void LayoutManager(){
         setResizable(true);
         setVisible(true);
@@ -35,6 +38,7 @@ public class menu extends JFrame implements ActionListener{
         setBounds(10,10,400,200);
     container.setLayout(null);
     }
+   //Komponen ditambahkan ke JFrame
     public void components(){
     container.add(menu1);
     container.add(menu2);
@@ -42,6 +46,7 @@ public class menu extends JFrame implements ActionListener{
     container.add(exit);
     container.add(text);
     }
+   //Letak letak komponen di JFrame
     public void LocationAndSize(){
     menu1.setBounds(40,40,150,50);
     menu2.setBounds(190,40,150,50);
@@ -49,6 +54,7 @@ public class menu extends JFrame implements ActionListener{
     text.setBounds(125,0,150,50);
     menu3.setBounds(40,80,150,50);
     }
+   //Penambahan action listener dan window listener
     public void actionevents(){
     menu1.addActionListener(this);
     menu2.addActionListener(this);
@@ -57,6 +63,7 @@ public class menu extends JFrame implements ActionListener{
     addWindowListener(new exitclass());
     }
     @Override
+   //ActionEvent handler
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==menu1){
             JOptionPane.showMessageDialog(this,"TES1");
@@ -74,6 +81,7 @@ public class menu extends JFrame implements ActionListener{
             }  
         }
     }
+   //WindowEvent handler
     private class exitclass extends WindowAdapter {
         @Override
         public void windowClosing(WindowEvent e){
